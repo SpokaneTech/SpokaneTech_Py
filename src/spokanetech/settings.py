138fc496c470170a74f787ac88fb89cd18c6f566
Joseph Riddle <joeriddles10@gmail.com>
2024-01-31 20:36:23 -0800

    Add dj_database_url

--- src/spokanetech/settings.py
+++ src/spokanetech/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,10 +75,11 @@ WSGI_APPLICATION = 'spokanetech.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:////{BASE_DIR}/db.sqlite3",
+        conn_max_age=600,
+        conn_health_checks=True,
+    ),
 }
 
 
