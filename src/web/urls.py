aa75689965bfd70b3be695cf5d04f2bf8752a97c
Joseph Riddle <joeriddles10@gmail.com>
2024-02-22 21:25:46 -0800

    Update index and events to use class based views

--- src/web/urls.py
+++ src/web/urls.py
@@ -5,11 +5,11 @@ from django.urls import path
 app_name = "web"
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('events', views.events, name="events"),
+    path('', views.Index.as_view(), name="index"),
+    path('events', views.ListEvents.as_view(), name="events"),
     path('groups', views.list_tech_groups, name="list_tech_groups"),
     path('groups/<int:pk>', views.get_tech_group, name="get_tech_group"),
-    path('events/<int:pk>', views.get_event, name="get_event"),
+    path('events/<int:pk>', views.DetailEvent.as_view(), name="get_event"),
     
     path('sidebar_events', views.GetEvents.as_view(), name="sidebar_events"),
     path('sidebar_groups', views.GetTechGroups.as_view(), name="sidebar_groups"),
--- src/web/views.py
+++ src/web/views.py
@@ -1,21 +1,46 @@
+from typing import Any
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.utils import timezone
 
+from django.views.generic import DetailView
+from handyhelpers.views.gui import HandyHelperListView, HandyHelperIndexView
 from handyhelpers.views.htmx import GenericHtmxView
 
-# import models
 from web.models import Event, TechGroup
 
 
-# Create your views here.
-def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "web/index.html")
+class Index(HandyHelperIndexView):
+    title = "Spokane Tech"
+    subtitle = "Index of Spokane's Tech User Groups"
+    base_template = "spokanetech/base.html"
+
+    def __init__(self, **kwargs: Any) -> None:
+        self.item_list = [
+            {
+                "url": tech_group.get_absolute_url(),
+                "icon": "fa-brands fa-python",  # TODO: add this to the TechGroup model
+                "title": str(tech_group),
+                "description": (tech_group.description or "")[:100],
+            }
+            for tech_group in
+            TechGroup.objects.all()
+        ]
+        super().__init__(**kwargs)
 
 
-def events(request: HttpRequest) -> HttpResponse:
-    events = Event.objects.all()
-    return HttpResponse("<br>".join(e.name for e in events))
+class ListEvents(HandyHelperListView):
+    title = "Events"
+    base_template = "spokanetech/base.html"
+    table = "web/partials/table/table_events.htm"
+
+    def __init__(self, **kwargs: Any) -> None:
+        self.queryset = Event.objects.filter(date_time__gte=timezone.now())
+        super().__init__(**kwargs)
+
+
+class DetailEvent(DetailView):
+    model = Event
 
 
 def list_tech_groups(request: HttpRequest) -> HttpResponse:
@@ -42,4 +67,7 @@ class GetTechGroups(GenericHtmxView):
 class GetEvents(GenericHtmxView):
     """Get a list of upcoming Events and render a partial to use on the sidebar navigation"""
     template_name = "web/partials/sidebar_items.htm"
-    queryset = Event.objects.filter(date_time__gte=timezone.now())
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.queryset = Event.objects.filter(date_time__gte=timezone.now())
