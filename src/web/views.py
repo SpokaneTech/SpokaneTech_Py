4fa74713083cabbe276dfb3ea12aaf9e41d01fa2
David Slusser <dbslusser@gmail.com>
2024-03-03 11:46:36 -0800

    adding calendar view

--- src/web/views.py
+++ src/web/views.py
@@ -2,11 +2,15 @@ from typing import Any
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.utils import timezone
+from django.template import loader
 
+# from django.urls import reverse
 from django.views.generic import DetailView
 from handyhelpers.views.gui import HandyHelperListView, HandyHelperIndexView
+from handyhelpers.views.calendar import CalendarView
 from handyhelpers.mixins.view_mixins import HtmxViewMixin
-from handyhelpers.views.htmx import BuildModelSidebarNav
+from handyhelpers.views.htmx import BuildModelSidebarNav, BuildBootstrapModalView
+
 
 from web.models import Event, TechGroup
 
@@ -24,8 +28,7 @@ class Index(HandyHelperIndexView):
                 "title": str(tech_group),
                 "description": (tech_group.description or "")[:100],
             }
-            for tech_group in
-            TechGroup.objects.all()
+            for tech_group in TechGroup.objects.all()
         ]
         super().__init__(**kwargs)
 
@@ -60,16 +63,45 @@ class DetailTechGroup(HtmxViewMixin, DetailView):
 
 def list_tech_groups(request: HttpRequest) -> HttpResponse:
     groups = TechGroup.objects.all()
-    return render(request, "web/list_tech_groups.html", { "groups": groups })
+    return render(request, "web/list_tech_groups.html", {"groups": groups})
 
 
 class BuildSidebar(BuildModelSidebarNav):
     """Get a list of upcoming Events and enabled TechGroups and render a partial to use on the sidebar navigation"""
+
     menu_item_list = [
-        {"queryset": Event.objects.filter(date_time__gte=timezone.now()),
-         "icon": """<i class="fa-solid fa-calendar-day"></i>""",
-         },
-        {"queryset": TechGroup.objects.filter(enabled=True),
-         "icon": """<i class="fa-solid fa-people-group"></i>""",
-         },
+        {
+            "queryset": Event.objects.filter(date_time__gte=timezone.now()),
+            "icon": """<i class="fa-solid fa-calendar-day"></i>""",
+            "list_all_url": "events",
+        },
+        {
+            "queryset": TechGroup.objects.filter(enabled=True),
+            "icon": """<i class="fa-solid fa-people-group"></i>""",
+        },
     ]
+
+
+class GetEventDetailsModal(BuildBootstrapModalView):
+    """get details of an event and display in a modal"""
+
+    modal_button_submit = None
+    modal_title = "Event Details"
+
+    def get(self, request, *args, **kwargs):
+        context = {}
+        context["object"] = Event.objects.get(pk=kwargs["pk"])
+        self.modal_subtitle = context["object"]
+        self.modal_body = loader.render_to_string(
+            "web/partials/modal/detail_event.htm", context=context
+        )
+        return super().get(request, *args, **kwargs)
+
+
+class EventCalendarView(CalendarView):
+    """Render a monthly calendar view of events"""
+
+    title = "Spokane Tech Event Calendar"
+    event_model = Event
+    event_model_date_field = "date_time"
+    event_detail_url = "web:get_event_details"
