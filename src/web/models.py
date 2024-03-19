a310df3df7d8bb4306aa33634d3bc8d320a1394a
Joseph Riddle <joeriddles10@gmail.com>
2024-03-19 00:26:51 -0700

    Add external_id to Event

--- src/web/models.py
+++ src/web/models.py
@@ -1,33 +1,9 @@
+from __future__ import annotations
+
 from django.db import models
 from django.urls import reverse
 
 from handyhelpers.models import HandyHelperBaseModel
-
-
-class Event(HandyHelperBaseModel):
-    """An event on a specific day and time."""
-
-    name = models.CharField(max_length=64, help_text="name of this event")
-    description = models.TextField(blank=True, null=True, help_text="name of this event")
-    date_time = models.DateTimeField(auto_now=False, auto_now_add=False, help_text="")
-    duration = models.IntegerField(blank=True, null=True, help_text="planned duration of this event")
-    location = models.CharField(
-        max_length=128,
-        blank=True,
-        null=True,
-        help_text="location where this event is being hosted",
-    )
-    group = models.ForeignKey("TechGroup", blank=True, null=True, on_delete=models.SET_NULL)
-    # labels = models.ManyToManyField("TechnicalArea")
-
-    # class Meta:
-    #     ordering = ("-created_at",)
-
-    def __str__(self) -> str:
-        return self.name  # type: ignore
-
-    def get_absolute_url(self) -> str:
-        return reverse("web:get_event", kwargs={"pk": self.pk})
 
 
 class TechGroup(HandyHelperBaseModel):
@@ -44,3 +20,35 @@ class TechGroup(HandyHelperBaseModel):
 
     def get_absolute_url(self) -> str:
         return reverse("web:get_tech_group", kwargs={"pk": self.pk})
+
+
+class Event(HandyHelperBaseModel):
+    """An event on a specific day and time."""
+
+    name = models.CharField(max_length=64, help_text="name of this event")
+    description = models.TextField(blank=True, null=True, help_text="name of this event")
+    date_time = models.DateTimeField(auto_now=False, auto_now_add=False, help_text="")
+    duration = models.IntegerField(blank=True, null=True, help_text="planned duration of this event")
+    location = models.CharField(
+        max_length=128,
+        blank=True,
+        null=True,
+        help_text="location where this event is being hosted",
+    )
+    external_id = models.CharField(
+        max_length=1024,
+        blank=True,
+        null=True,
+        help_text="ID field for tracking a unique external event",
+    )
+    group = models.ForeignKey(TechGroup, blank=True, null=True, on_delete=models.SET_NULL)
+    # labels = models.ManyToManyField("TechnicalArea")
+
+    # class Meta:
+    #     ordering = ("-created_at",)
+
+    def __str__(self) -> str:
+        return self.name  # type: ignore
+
+    def get_absolute_url(self) -> str:
+        return reverse("web:get_event", kwargs={"pk": self.pk})
