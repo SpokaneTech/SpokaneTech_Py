0c6d39d63903bdb47f2d40471317473bd5acde28
Joseph Riddle <joeriddles10@gmail.com>
2024-03-18 21:52:25 -0700

    Refactor discord task into service for testing

--- src/web/tasks.py
+++ src/web/tasks.py
@@ -1,31 +1,14 @@
-from datetime import timedelta
-
 from celery import shared_task
 from discord import SyncWebhook
 
 from django.conf import settings
-from django.utils import timezone
 
-from web import models
+from web import services
 
 
 @shared_task()
 def send_events_to_discord():
     """Send upcoming events to the Discord server."""
     webhook = SyncWebhook.from_url(settings.DISCORD_WEBHOOK_URL)
-    today = timezone.localdate()
-    events = models.Event.objects.filter(
-        date_time__gte=today,
-        date_time__lt=today + timedelta(days=7),
-    ).select_related("group").order_by("date_time")
-
-    message = "_Here are the upcoming Spokane Tech events for this week:_\n\n"
-    for event in events:
-        event_url = event.url if event.url else f"https://spokanetech.org{event.get_absolute_url()}"
-        event_msg = f"**{event.date_time.strftime('%A, %b %d @ %-I:%M %p')}**\n"
-        if event.group:
-            event_msg += f"{event.group.name} — "
-        event_msg += f"[{event.name}](<{event_url}>)"
-        message += event_msg + "\n\n"
-
-    webhook.send(message)
+    service = services.DiscordService(webhook)  # type: ignore
+    service.send_events()
