40c3a1a4ca6805231e98af586070e82af75e2654
Joseph Riddle <joeriddles10@gmail.com>
2024-03-20 09:59:06 -0700

    Replace Empty singleton type with None

--- src/web/services.py
+++ src/web/services.py
@@ -1,4 +1,3 @@
-from enum import Enum
 from typing import Protocol
 from datetime import timedelta
 
@@ -7,22 +6,15 @@ from django.utils import timezone
 
 from web import models, scrapers
 
-class Empty(Enum):
-    """See https://peps.python.org/pep-0484/#support-for-singleton-types-in-unions"""
-    token = 0
-
-_empty = Empty.token
-
 
 class MeetupService:
-
     def __init__(
         self,
-        homepage_scraper: scrapers.Scraper[list[str]] | Empty = _empty,
-        event_scraper: scrapers.Scraper[models.Event] | Empty = _empty,
+        homepage_scraper: scrapers.Scraper[list[str]] | None = None,
+        event_scraper: scrapers.Scraper[models.Event] | None = None,
     ) -> None:
-        self.homepage_scraper: scrapers.Scraper[list[str]] = homepage_scraper if not isinstance(homepage_scraper, Empty) else scrapers.MeetupHomepageScraper()
-        self.event_scraper: scrapers.Scraper[models.Event] = event_scraper if not isinstance(event_scraper, Empty) else scrapers.MeetupEventScraper()
+        self.homepage_scraper: scrapers.Scraper[list[str]] = homepage_scraper or scrapers.MeetupHomepageScraper()
+        self.event_scraper: scrapers.Scraper[models.Event] = event_scraper or scrapers.MeetupEventScraper()
 
     def scrape_events_from_meetup(self) -> None:
         """Scrape upcoming events from Meetup and save them to the database."""
@@ -38,6 +30,7 @@ class MeetupService:
 
 class Sender(Protocol):
     def send(self, message: str, **kwargs) -> None:
+        """Send a message somewhere."""
         ...
 
 
