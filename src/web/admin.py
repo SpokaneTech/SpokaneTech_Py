ebc0079cb0e560d3954cefc36ca9a35c77249ced
Joseph Riddle <joeriddles10@gmail.com>
2024-03-27 08:13:04 -0700

    Remove description from admin UI

--- src/web/admin.py
+++ src/web/admin.py
@@ -1,23 +1,21 @@
 from django.contrib import admin
 
-# import models
 from web.models import (Event,
                         TechGroup
                         )
 
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'description', 'date_time', 'duration', 'location', 'group', 'created_at', 'updated_at']
-    search_fields = ['id', 'name', 'description', 'duration', 'location']
+    list_display = ['id', 'name', 'date_time', 'duration', 'location', 'group', 'created_at', 'updated_at']
+    search_fields = ['id', 'name', 'duration', 'location']
     list_filter = ['group']
 
 
 class TechGroupAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'description', 'enabled', 'homepage', 'created_at', 'updated_at']
-    search_fields = ['id', 'name', 'description', 'homepage']
+    list_display = ['id', 'name', 'enabled', 'homepage', 'created_at', 'updated_at']
+    search_fields = ['id', 'name', 'homepage']
     list_filter = ['enabled']
 
 
-# register models
 admin.site.register(Event, EventAdmin)
 admin.site.register(TechGroup, TechGroupAdmin)
