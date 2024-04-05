bab41f505c0a65b5eab3aef03c7ef226da73f685
Joseph Riddle <joeriddles10@gmail.com>
2024-04-04 21:50:17 -0700

    Set and use the user's timezone in sesson storage

--- src/spokanetech/settings.py
+++ src/spokanetech/settings.py
@@ -79,6 +79,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "web.middleware.TimezoneMiddleware",
 ]
 
 ROOT_URLCONF = "spokanetech.urls"
--- src/web/urls.py
+++ src/web/urls.py
@@ -6,6 +6,7 @@ app_name = "web"
 
 urlpatterns = [
     path("", views.Index.as_view(), name="index"),
+    path("set_timezone/", views.set_timezone, name="set_timezone"),
     path("events", views.ListEvents.as_view(), name="events"),
     path("groups", views.list_tech_groups, name="list_tech_groups"),
     path("groups/<int:pk>", views.DetailTechGroup.as_view(), name="get_tech_group"),
