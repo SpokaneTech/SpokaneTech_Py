9106f19fff9f457cb355e280cfafffbe03d9a549
joeriddles <joeriddles10@gmail.com>
2024-08-14 21:33:22 -0700

    Add envvars to SSH in Docker container

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 set -eux
+
+# Get env vars in the Dockerfile to show up in the SSH session
+# See also: https://azureossd.github.io/2022/04/27/2022-Enabling-SSH-on-Linux-Web-App-for-Containers/index.html
+eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
+
 service ssh start
 exec "$@"
