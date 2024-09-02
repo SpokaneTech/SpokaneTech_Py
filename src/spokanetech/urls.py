decaa1e7ff646320affaa3429c196a2d6e27e3b9
David Slusser <david.slusser@sealstorage.io>
2024-09-02 09:19:35 -0700

    adding media to urls

--- src/spokanetech/urls.py
+++ src/spokanetech/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -23,7 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("handyhelpers/", include("handyhelpers.urls")),
     path("", include("web.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 if settings.DEBUG:
