dfa19fb9c2d978696080410cf50e4fd519093479
Joseph Riddle <joeriddles10@gmail.com>
2024-09-16 17:39:32 -0700

    Remove collectstatic from deployment for now

--- scripts/post_deploy.sh
+++ scripts/post_deploy.sh
@@ -5,4 +5,4 @@ python manage.py migrate
 
 # `node_modules` static folder is huge so ignore it
 # If `node_modules` is updated, run collectstatic manually
-python manage.py collectstatic --noinput -v=2 --ignore node_modules
+# python manage.py collectstatic --noinput -v=2 --ignore node_modules
