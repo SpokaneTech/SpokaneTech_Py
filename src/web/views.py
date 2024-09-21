6921a73193ac1b7a00fea0621ba5094a17efb8e0
David Slusser <david.slusser@sealstorage.io>
2024-09-21 16:34:08 -0700

    minor code cleanup

--- src/web/views.py
+++ src/web/views.py
@@ -51,62 +51,6 @@ class RequireStaffMixin(UserPassesTestMixin):
         return user.is_authenticated and user.is_staff  # type: ignore
 
 
-class ListEvents(CanEditMixin, HtmxViewMixin, HandyHelperListPlusFilterView):
-    title = "Events"
-    base_template = "spokanetech/base.html"
-    template_name = "web/event_list.html"
-
-    filter_form_obj = forms.ListEventsFilter
-    filter_form_url = reverse_lazy("web:filter_list_view")
-
-    def __init__(self, **kwargs: Any) -> None:
-        self.queryset = (
-            Event.objects.filter(date_time__gte=timezone.localtime())
-            .select_related("group")
-            .prefetch_related("tags", "group__tags")
-        )
-        super().__init__(**kwargs)
-
-    def get(self, request, *args, **kwargs):
-        if self.is_htmx():
-            self.template_name = "web/partials/event_list.htm"
-        return super().get(request, *args, **kwargs)
-
-    def filter_by_query_params(self):
-        """Include events that don't have any tags of their own but their group tags match."""
-        queryset = super().filter_by_query_params()
-        if queryset is None:
-            return None
-
-        if tags := self.request.GET.getlist("tags"):
-            events_with_group_tags_queryset = Event.objects.filter(
-                date_time__gte=timezone.localtime()
-            ).filter_group_tags(tags)  # type: ignore
-            queryset = queryset | events_with_group_tags_queryset
-
-        queryset = queryset.order_by("date_time")
-        return queryset
-
-
-class DetailEvent(HtmxViewMixin, DetailView):
-    model = Event
-
-    def __init__(self, **kwargs: Any) -> None:
-        self.queryset = Event.objects.select_related("group").prefetch_related("tags", "group__tags")
-        super().__init__(**kwargs)
-
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        context = super().get_context_data(**kwargs)  # type: ignore
-        user = self.request.user
-        context["can_edit"] = user.is_authenticated and user.is_staff  # type: ignore
-        return context
-
-    def get(self, request, *args, **kwargs):
-        if self.is_htmx():
-            self.template_name = "web/partials/detail_event.htm"
-        return super().get(request, *args, **kwargs)
-
-
 class CreateEvent(CreateView):
     model = Event
 
@@ -143,42 +87,6 @@ class UpdateEvent(RequireStaffMixin, UpdateView):
         return super().get_success_url()
 
 
-class DetailTechGroup(HtmxViewMixin, DetailView):
-    model = TechGroup
-
-    def __init__(self, **kwargs: Any) -> None:
-        self.queryset = TechGroup.objects.prefetch_related(
-            Prefetch("event_set", Event.objects.filter(date_time__gte=timezone.localtime()))
-        )
-        super().__init__(**kwargs)
-
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        context = super().get_context_data(**kwargs)
-        user = self.request.user
-        context["can_edit"] = user.is_authenticated and user.is_staff  # type: ignore
-        return context
-
-    def get(self, request, *args, **kwargs):
-        if self.is_htmx():
-            self.template_name = "web/partials/detail_tech_group.htm"
-        return super().get(request, *args, **kwargs)
-
-
-class ListTechGroup(CanEditMixin, HtmxViewMixin, HandyHelperListView):
-    title = "Tech Groups"
-    base_template = "spokanetech/base.html"
-    template_name = "web/techgroup_list.html"
-
-    def __init__(self, **kwargs: Any) -> None:
-        self.queryset = TechGroup.objects.filter(enabled=True)
-        super().__init__(**kwargs)
-
-    def get(self, request, *args, **kwargs):
-        if self.is_htmx():
-            self.template_name = "web/partials/techgroup_list.htm"
-        return super().get(request, *args, **kwargs)
-
-
 class CreateTechGroup(RequireStaffMixin, CreateView):
     model = TechGroup
     form_class = forms.TechGroupForm
@@ -195,39 +103,8 @@ class EventCalendarView(HtmxCalendarView):
     event_model = Event
     event_model_date_field = "date_time"
     event_detail_url = "web:techevent_modal"
-    # template_name = "web/partials/custom/calendar.htm"
     htmx_template_name = "web/partials/custom/calendar.htm"
     template_name = "web/full/custom/calendar.html"
-
-
-class FilterListView(View):
-    """apply filters, as provided via queryparameters, to a list view that uses the FilterByQueryParamsMixin"""
-
-    def post(self, request, *args, **kwargs):
-        """process POST request
-
-        **This handles multiple values for the same filter key
-        which Handy Helpers does not currently do.**
-        """
-        redirect_url = self.request.META["HTTP_REFERER"]
-        form_parameters = self.request.POST
-
-        # build filtered URL
-        filter_url = f"{redirect_url.split('?')[0]}?"
-        for key in form_parameters:
-            # remove csrf token from POST parameters
-            if key == "csrfmiddlewaretoken":
-                continue
-
-            values = form_parameters.getlist(key)
-            for value in values:
-                filter_url += f"{key}={value}&"
-
-        return redirect(filter_url)
-
-
-
-
 
 
 class GetAboutContent(HtmxOptionView):
