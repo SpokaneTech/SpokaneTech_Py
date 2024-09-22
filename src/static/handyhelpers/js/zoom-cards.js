b267c22eb71ce437eff5d452e441b7e32a006148
David Slusser <david.slusser@sealstorage.io>
2024-09-21 17:35:23 -0700

    nuke static

--- src/static/handyhelpers/js/zoom-cards.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-    $("div.zoom-card").hover(function () {
-        $(this).toggleClass('scale-up').siblings().toggleClass('scale-down')
-    });
-
-    $("a.zoom-card").hover(function () {
-        $(this).toggleClass('scale-up').siblings().toggleClass('scale-down')
-    });
-});
