ef55f34d1f6b3f3fa882bfe9dd03a5b43c97a3fe
David Slusser <david.slusser@sealstorage.io>
2024-09-21 17:11:28 -0700

    removing extentions; removing debugtoolbar settings

--- src/spokanetech/settings.py
+++ src/spokanetech/settings.py
@@ -85,7 +85,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "storages",
-    "django_extensions",
     "django_celery_results",
     "django_celery_beat",
     "django_filters",
@@ -283,26 +282,6 @@ MARKDOWNIFY = {
         ]
     },
 }
-
-
-# Django Debug Toolbar
-# DEBUG_TOOLBAR_CONFIG = {
-#     "DISABLE_PANELS": {
-#         "debug_toolbar.panels.cache.CachePanel",
-#         "debug_toolbar.panels.headers.HeadersPanel",
-#         "debug_toolbar.panels.history.HistoryPanel",
-#         "debug_toolbar.panels.profiling.ProfilingPanel",
-#         "debug_toolbar.panels.redirects.RedirectsPanel",
-#         "debug_toolbar.panels.request.RequestPanel",
-#         "debug_toolbar.panels.settings.SettingsPanel",
-#         "debug_toolbar.panels.signals.SignalsPanel",
-#         "debug_toolbar.panels.sql.SQLPanel",
-#         "debug_toolbar.panels.staticfiles.StaticFilesPanel",
-#         "debug_toolbar.panels.templates.TemplatesPanel",
-#         "debug_toolbar.panels.timer.TimerPanel",
-#         "debug_toolbar.panels.versions.VersionsPanel",
-#     }
-# }
 
 
 # Crispy Forms
