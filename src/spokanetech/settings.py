e1b4581fbf3d52bd6a8cdf5ded84772880601f88
David Slusser <david.slusser@sealstorage.io>
2024-09-24 22:21:10 -0700

    minor code cleanup

--- src/spokanetech/settings.py
+++ src/spokanetech/settings.py
@@ -190,7 +190,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(str(BASE_DIR), "staticroot")
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
